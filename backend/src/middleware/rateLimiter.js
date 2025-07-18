d26b290dda79451710ef3bb3dcb81958ac691595
KorawichInt <korawich.inthamusik@gmail.com>
2025-07-18 17:22:24 +0700

    initial commit

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,17 @@
+import ratelimit from "../config/upstash.js";
+
+const rateLimiter = async (req, res, next) => {
+  try {
+    const identifier = "userID";
+    const { success } = await ratelimit.limit(identifier);
+    if (!success) {
+      return res.status(429).json({ message: "Too many requests" });
+    }
+    next();
+  } catch (error) {
+    console.log("Rate limit error", error);
+    next(error);
+  }
+};
+
+export default rateLimiter;
